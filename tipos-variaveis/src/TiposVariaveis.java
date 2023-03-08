6a4341e791d772e2d1fecd755b6b8292ebd9c064
deisekinsk <kinsk@msn.com>
2023-03-08 05:10:23 -0300

    Convenções dos tipos e variáveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,36 @@
+/**
+ * https://glysns.gitbook.io/java-basico/
+ * Java possui oito tipos primitivos:
+ * int (4 byte), byte (1 byte), short (2 byte), long (8 byte), float (4 byte), double (8 byte), boolean e chart. São armazenados na pilha de memória (memori stack). A JVM por padrão tenta converte o valor para int. Por isso o 
+ */
+
+ /**
+ * Todo arquivo começa com letra maiúscula, sem espaço com camelCase:
+ * ProjectOne.java
+ * 
+ * A classe possui o mesmo nome que o arquivo.
+ * 
+ * As variáveis são escritas com minúscula, sem espaço, com camelCase. É permitido utilizar _ e $ para concatenação. As variáveis se trata de um valor que está/estará na memória. Estrutura:]
+ *      tipo nomeBemDefinido = Atribuição E/OU Não;
+ * 
+ * Palavras Reservadas:
+ * final: não deixa mudar a variável. Valor constante. 
+ */
+
+
 public class TiposVariaveis {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+    final String BR = "Brasil";
+    double PI = 3.14F; //final
+    long quantidade = 2222255566L; //termina com L
+    int ANO_2000 = 2000;
+    int ESTADOS_BRASILEIRO = 27; //valores que iniciam com zero usar outro tipo
+
+    boolean casaPropria;
+    boolean estaLigado = false;    
+
+    //converção
+    short shortNumber = 1;
+    int normalNumber = shortNumber;
+    short shortNumber2 = (short) normalNumber;
+
 }
